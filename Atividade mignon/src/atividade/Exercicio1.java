295755d3b2a9b9d123219dfe882af4fc33d22079
Evelyn Carmo <evelyncarmo95@gmail.com>
2020-05-21 22:47:24 -0300

    Calculo centro da gravidade ok, falta só a matriz

--- Atividade mignon/src/atividade/Exercicio1.java
+++ Atividade mignon/src/atividade/Exercicio1.java
@@ -10,116 +10,170 @@ package atividade;
  * @author vyvis
  */
 public class Exercicio1 {
+
     public static void main(String[] args) {
-                
-                //faz a matriz soma na posiçã ij receber  matriz 1 na posição ij + matriz 2 na posição ij e depois imprimi 
-                /*a posição soma[i][j] que contem a soma das duas posições me cada matriz
-                soma[i][j] = m1[i][j]+m2[i][j];
-                System.out.print(soma[i][j]+" ");
-            }
-            System.out.println();
-        }*/
-        
-        
-        int ql = 3;
-        int qc = 3;
-        
-       double matriz2 [][] = new double [ql][qc];
-        
-       double [][] matriz = {{0.1,0.2,0.1,0.2,0.1},
-                             {0.1,0.2,0.3,0.1,0.1},
-                             {0.2,0.3,0.1,0.1,0.3},
-                             {0.4,0.1,0.1,0.1,0.2},
-                             {0.4,0.2,0.2,0.3,0.1}};
 
-//       double [][] matriz = {{0.1,0.1,0.1,0.1,0.1,0.1,0.1,0.1,0.1,0.1},
-//                             {0.2,0.2,0.2,0.2,0.2,0.2,0.2,0.2,0.2,0.2},
-//                             {0.3,0.3,0.3,0.3,0.3,0.3,0.3,0.3,0.3,0.3},
-//                             {0.4,0.4,0.4,0.4,0.4,0.4,0.4,0.4,0.4,0.4},
-//                             {0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5}};
+        int ql = 5; // quantidade de linha
+        int qc = 10; // quantidade de coluna
 
-       
-       
-       
+        double matriz2[][] = new double[ql][qc];
+        
+        /*double[][] matriz = {{0.1, 0.2, 0.1, 0.2, 0.1},
+        {0.1, 0.2, 0.3, 0.1, 0.1},
+        {0.2, 0.3, 0.1, 0.1, 0.3},
+        {0.4, 0.1, 0.1, 0.1, 0.2},
+        {0.4, 0.2, 0.2, 0.3, 0.1}};*/
+
+       double [][] matriz = {{0.1,0.1,0.1,0.1,0.1,0.1,0.1,0.1,0.1,0.1},
+                             {0.2,0.2,0.2,0.2,0.2,0.2,0.2,0.2,0.2,0.2},
+                            {0.3,0.3,0.3,0.3,0.3,0.3,0.3,0.3,0.3,0.3},
+                             {0.4,0.4,0.4,0.4,0.4,0.4,0.4,0.4,0.4,0.4},
+                             {0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5}};   
+        
+        
+        
         int cgl = gravidadeLinha(matriz, qc, ql);
-        
-        System.out.println(cgl);
-       
+        int cgc = gravidadeColuna(matriz,qc,ql);
+        System.out.println("Centro da gravidade : "+cgl + " " + cgc);
     }
+
     
-    public static int gravidadeLinha( double [][] m, int c, int l){
-        double menor=0; // recebe a soma de todas as posições para comparar depois
-        int rodada=1; // variavel para prender o laço do while
-        int cgl=0; //variavel que vai retornar a linha do centro da gravidade
-        double pl1=0; // porção linha 1
-        double pl2=0; // porção linha 2
-        
+    public static int gravidadeLinha(double[][] m, int c, int l) {
+        double menor = 0; // recebe a soma de todas as posições para comparar depois
+        int rodada = 1; // variavel para prender o laço do while
+        int ignorada = l - rodada; // recebe a linha a ser "testada"
+
+        int cgl = 0; //variavel que vai retornar a linha do centro da gravidade
+
         // realiza a soma de todas as posições para saber qual o maior maximo, para comparar com o calculo das porções
-        for(int i=0; i < l; i++){
-            for(int j =0; j < c; j++){
+        for (int i = 0; i < l; i++) {
+            for (int j = 0; j < c; j++) {
                 menor += m[i][j];
             }
         }
-        
-        //enquanto minha rodada nao for igual ao numero de linhas
-        while(rodada < l){
-            int ignorada; // recebe a linha a ser "testada"
-            int aux2 = l-1; // recebe a qauntidade de linhas - 1
-            
-            
+
+        //enquanto minha rodada for menor que o numero de linhas
+        while (rodada < l) {
+            double pl1 = 0; // vai receber a primeira porção 
+            double pl2 = 0; // vai receber a segunda porção
+
             // contador para entrar nas linhas
-            for(int i =0; i < l; i++){
-                
-                ignorada = l - rodada; // recebe a quantidade de linhas menos o valor da rodada
-                
-                    //se a linha percorrida for a linha que está sendo "testada"
-                    if(i == ignorada){
-                        
-                        
-                        // contador pra percorrer da quantidade de linhas até a linha ignorada
-                        for(int ii = aux2; ii > ignorada; ii--){
-                            // percorre as colunas
-                            for(int jj = 0; jj < c; jj++){
-                            // recebe a soma do numero armazenado na linha e todas as colunas    
-                            pl2 += m[ii][jj];
-                            }
-                        }
-                        
-                        /*Se a porção 2 for maior que a porção 1 ele faz a troca para que o 
-                        valor não seja negativo*/
-                        if (pl2 > pl1) {
-                            double aux3 = pl2;
-                            pl2 = pl1;
-                            pl1 = aux3;
-                        }
-                        
-                        // Faz a primeira porçao menos a segunda porçao
-                       double aux = pl1 - pl2;
-                       
-                       // se o resultado da subtração das porções for menor, do que a ultima
-                       if(aux < menor){
-                           //copia o resultado nela
-                           menor = aux;
-                           // e significa que a linha testada é o centro da gravidade
-                           cgl=ignorada;
-                       }
-                       pl1=0; //tira o resultado dos ultimos calculos
-                       pl2=0;//tira o resultado dos ultimos calculos
-                       rodada++; // adciona a rodada
-                       /*Obs Léo: esse i-0 é necessário? Pois quando executa esse comando, teoricamente ele sai do laço*/
-                       i=0;// e zera o contador das linhas
-                        
-                    }else{
-                        //se a linha não for a linha que será testada, faz a variavel que recebe a soma da primeira porção, receber a soma dela.
-                        for(int j = 0; j < c; j++){
-                            pl1+=m[i][j];
+            for (int i = 0; i < l; i++) {
+
+                //se a linha percorrida for a linha que está sendo "testada"
+                if (i == ignorada) {
+
+                    // contador pra percorrer da quantidade de linhas até a linha ignorada
+                    for (int ii = l-1; ii > ignorada; ii--) {
+                        for (int j = 0; j < c; j++) {
+                            pl2 += m[ii][j];// recebe a soma do numero armazenado na linha e todas as colunas    
                         }
                     }
-            }  
+
+                    /*Se a porção 2 for maior que a porção 1 ele faz a troca para que o 
+                        valor não seja negativo*/
+                    if (pl2 > pl1) {
+                        double aux = pl2;
+                        pl2 = pl1;
+                        pl1 = aux;
+                    }
+
+                    // Faz o calculo 
+                    double aux2 = pl1 - pl2;
+
+                    // se o resultado da subtração das porções for menor, do que a ultima
+                    if (aux2 < menor) {
+                        menor = aux2;//copia o resultado nela
+
+                        cgl = ignorada;// e significa que a linha testada é o centro da gravidade
+                    }
+
+                    pl1 = 0; //tira o resultado dos ultimos calculos
+                    pl2 = 0;//tira o resultado dos ultimos calculos
+                    rodada++; // adciona a rodada
+                    ignorada--;
+                    /*Obs Léo: esse i-0 é necessário? Pois quando executa esse comando, teoricamente ele sai do laço*/
+                    i = -1;// e zera o contador das linhas
+
+                } else {
+                    //se a linha não for a linha que será testada, faz a variavel que recebe a soma da primeira porção, receber a soma dela.
+                    for (int j = 0; j < c; j++) {
+                        pl1 += m[i][j];
+                    }
+                }
+            }
         }
-        
+
         // retorna a linha que é o centro da gravidade
-        return cgl;
+        return cgl+1;   
     }
     
+    public static int gravidadeColuna(double[][] m, int c, int l) {
+        double menor = 0; // recebe a soma de todas as posições para comparar depois
+        int rodada = 1; // variavel para prender o laço do while
+        int ignorada = c - rodada; // recebe a linha a ser "testada"
+
+        int cgc = 0; //variavel que vai retornar a linha do centro da gravidade
+
+        // realiza a soma de todas as posições para saber qual o maior maximo, para comparar com o calculo das porções
+        for (int i = 0; i < l; i++) {
+            for (int j = 0; j < c; j++) {
+                menor += m[i][j];
+            }
+        }
+
+        //enquanto minha rodada for menor que o numero de colunas
+        while (rodada < c) {
+            double pl1 = 0; // vai receber a primeira porção 
+            double pl2 = 0; // vai receber a segunda porção
+
+            // contador para entrar nas coluna
+            for (int i = 0; i < c; i++) {                
+                //se a coluna percorrida for a coluna que está sendo "testada"
+                if (i == ignorada) {                 
+                    // contador pra percorrer da quantidade de linhas até a linha ignorada
+                    for (int j = 0; j < l; j++) {
+                        for (int ii = c-1; ii > ignorada; ii--) {
+                            pl2 += m[j][ii];// recebe a soma do numero armazenado na linha e todas as colunas    
+                        }
+                    }
+
+                    /*Se a porção 2 for maior que a porção 1 ele faz a troca para que o 
+                        valor não seja negativo*/
+                    if (pl2 > pl1) {
+                        double aux = pl2;
+                        pl2 = pl1;
+                        pl1 = aux;
+                    }
+
+                    // Faz o calculo 
+                    double aux2 = pl1 - pl2;
+
+                    // se o resultado da subtração das porções for menor, do que a ultima
+                    if (aux2 < menor) {
+                        menor = aux2;//copia o resultado nela
+
+                        cgc = ignorada;// e significa que a linha testada é o centro da gravidade
+                    }
+
+                    pl1 = 0; //tira o resultado dos ultimos calculos
+                    pl2 = 0;//tira o resultado dos ultimos calculos
+                    rodada++; // adciona a rodada
+                    ignorada--;
+                    /*Obs Léo: esse i-0 é necessário? Pois quando executa esse comando, teoricamente ele sai do laço*/
+                    i = -1;// e zera o contador das linhas
+
+                } else { // OK, CORRETO
+                    //se a linha não for a coluna que será testada, faz a variavel que recebe a soma da primeira porção, receber a soma dela.
+                    for (int j = 0; j < l; j++) {
+                        pl1 += m[j][i];
+                    }
+                }
+            }
+        }
+
+        // retorna a linha que é o centro da gravidade
+        return cgc+1;   
+    }
+
 }
